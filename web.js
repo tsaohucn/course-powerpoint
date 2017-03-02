632082ca2fd86c2745bf1f4c48f63f29e47b5ff4
Tsao <tsaohucn@gmail.com>
2017-03-02 22:33:52 +0800

    modify web.js

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-app.use(express.logger());
-app.use("/", express.static(__dirname));
-
 var port = process.env.PORT || 5000;
+
+app.use(express.logger());
+app.use('/', express.static(__dirname));
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log('Server started on ' + port);
 });
